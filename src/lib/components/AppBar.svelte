43acbec3035bcc657528ff0f4e0378f793304982
Ryan Joshua <ryanjoshuagapac@gmail.com>
2025-07-21 10:32:25 +0800

    new things

---
+++ src/lib/components/AppBar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Logo from '$lib/assets/logo-transparent.png';
+  
+  export let title = "Portfolio";
+</script>
+
+<header class="w-full">
+  <nav class="flex p-8 justify-between items-center">
+    <img src={Logo} class="w-32" alt={title} />
+    <div class="flex gap-2">
+      <slot />
+    </div>
+  </nav>
+</header>
+
+<style>
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
-	import '../app.css';
-	
-	let { children } = $props();
+  import AppBar from '$lib/components/AppBar.svelte';
+  import ShaderCanvas from '$lib/components/ShaderCanvas.svelte';
+  import '../app.css';
+
+  let { children } = $props();
+
+  export const prerender = true;
 </script>
 
-{@render children()}
+<div class="background-container">
+  <ShaderCanvas />
+</div>
+
+<div class="content-container">
+  <AppBar>
+    <a href="/about">About Me</a>
+  </AppBar>
+  {@render children()}
+</div>
+
+<style>
+  .background-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: -1;
+  }
+
+  .content-container {
+    position: relative;
+    z-index: 1;
+  }
+</style>
